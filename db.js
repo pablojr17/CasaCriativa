39dc422a717762246572f9638198f1b06df3d355
pablojr17 <pablojr17@gmail.com>
2020-04-01 15:53:50 -0300

    Projeto Casa Criativa

---
+++ db.js
@@ -0,0 +1,56 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./ws.db');
+
+db.serialize(function() {
+
+  // db.run(`
+  //   CREATE TABLE IF NOT EXISTS ideas(
+  //     id INTEGER PRIMARY KEY AUTOINCREMENT,
+  //     image TEXT,
+  //     title TEXT,
+  //     category TEXT,
+  //     description TEXT,
+  //     link TEXT
+  //   );
+  // `)
+  
+  // const query = `
+  //     INSERT INTO ideas(
+  //       image,
+  //       title,
+  //       category,
+  //       description,
+  //       link
+  //     ) VALUES (?,?,?,?,?);    
+  // `
+
+  // const values = [
+  //   "https://image.flaticon.com/icons/svg/478/478594.svg",
+  //   "Curso de Programação - Rocketseat",
+  //   "Estudo",
+  //   "No meio de tanta informação e da quantidade de ferramentas que surgem todos os dias, você precisa de alguém que te leve na direção certa.",
+  //   "https://rocketseat.com.br/starter"
+  // ]
+
+  // db.run (query, values, function(err) {
+  //   if (err) return console.log(err)
+
+  //   console.log(this)
+  // })
+
+  //DELETAR DADOS
+  // db.run(`DELETE FROM ideas WHERE id = ?`, [1], function(err) {
+  //   if (err) return console.log(err)
+
+  //   console.log("DELETEI", this)
+  // })
+
+  //CONSULTA DADOS
+  //  db.all(`SELECT * FROM ideas`, function(err, rows) {
+  //    if (err) return console.log(err)
+
+  //    console.log(rows)
+  //  })
+})
+
+module.exports = db;
